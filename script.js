59c9f732715eba81c566ffbd29d992d04f3ac00c
feliciaseg <felicia.segerholm97@gmail.com>
2021-03-07 21:59:46 +0100

    links added

--- script.js
+++ script.js
@@ -1,70 +1,70 @@
+window.onload = start;
+//GLOBAL VARIABLES
+const textArray = [
+  "HTML5",
+  "agila metoder",
+  "React Native",
+  "CSS",
+  "Typescript",
+  "Scrum",
+  "Bootstrap",
+  "Git",
+  "Objekt-orienterad programmering",
+  "Kanban",
+  "Javascript",
+  "React",
+  "P5.js",
+  "JSDoc",
+];
+const typingDelay = 100;
+const erasingDelay = 50;
+const newTextDelay = 1000;
+let textArrayIndex = 0;
+let charIndex = 0;
 
-
-// window.onload = start;
-
-// function start() {
-//     fadeIn();
-    
-    
-// }
+function start() {
+  typingEffectProject();
+  fadeIn();
+  let scroll = new SmoothScroll('a[href*="#"]');
+  console.log("hello?");
+}
 
 function fadeIn() {
-    const fade = document.getElementById("startSection");
-     let opacity = 0;
-     const intervalID = setInterval (function() {
-         if (opacity < 1) {
-             opacity = opacity + 0.2
-             fade.style.opacity = opacity;
-         } else {
-             clearInterval (intervalID);
-         }
-     }, 100); 
- }
+  const fade = document.getElementById("startSection");
+  let opacity = 0;
+  const intervalID = setInterval(function () {
+    if (opacity < 1) {
+      opacity = opacity + 0.2;
+      fade.style.opacity = opacity;
+    } else {
+      clearInterval(intervalID);
+    }
+  }, 100);
+}
 
+function typingEffectProject() {
+  const typedTextSpan = document.getElementById("typed-text");
+  if (charIndex < textArray[textArrayIndex].length) {
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(typingEffectProject, typingDelay);
+  } else {
+    setTimeout(erase, newTextDelay);
+  }
+}
 
- const items = document.getElementsByClassName('img-slider');
- const itemCount = items.length;
- const nextItem = document.querySelector('.next');
- const previousItem = document.querySelector('.previous');
- let count = 0;
- 
-
- function showNextItem() {
-   items[count].classList.remove('active');
- 
-   if(count < itemCount - 1) {
-     count++;
-   } else {
-     count = 0;
-   }
- 
-   items[count].classList.add('active');
-   console.log(count);
- }
- 
- function showPreviousItem() {
-   items[count].classList.remove('active');
- 
-   if(count > 0) {
-     count--;
-   } else {
-     count = itemCount - 1;
-   }
- 
-   items[count].classList.add('active');
-   console.log(count);
- }
- 
- function keyPress(e) {
-   e = e || window.event;
-   
-   if (e.keyCode == '37') {
-     showPreviousItem();
-   } else if (e.keyCode == '39') {
-     showNextItem();
-   }
- }
- 
- nextItem.addEventListener('click', showNextItem);
- previousItem.addEventListener('click', showPreviousItem);
- document.addEventListener('keydown', keyPress);
+function erase() {
+  const typedTextSpan = document.getElementById("typed-text");
+  if (charIndex > 0) {
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(
+      0,
+      charIndex - 1
+    );
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } else {
+    textArrayIndex++;
+    if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+    setTimeout(typingEffectProject, typingDelay + 1100);
+  }
+}
