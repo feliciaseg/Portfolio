e9503acabe3aa0a6be7076455177325e09addcd3
feliciaseg <felicia.segerholm97@gmail.com>
2021-03-07 15:08:39 +0100

    image slider 4 projects added

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 
 
-window.onload = start;
+// window.onload = start;
 
-function start() {
-    fadeIn();
-
-}
+// function start() {
+//     fadeIn();
+    
+    
+// }
 
 function fadeIn() {
     const fade = document.getElementById("startSection");
@@ -19,3 +20,51 @@ function fadeIn() {
          }
      }, 100); 
  }
+
+
+ const items = document.getElementsByClassName('img-slider');
+ const itemCount = items.length;
+ const nextItem = document.querySelector('.next');
+ const previousItem = document.querySelector('.previous');
+ let count = 0;
+ 
+
+ function showNextItem() {
+   items[count].classList.remove('active');
+ 
+   if(count < itemCount - 1) {
+     count++;
+   } else {
+     count = 0;
+   }
+ 
+   items[count].classList.add('active');
+   console.log(count);
+ }
+ 
+ function showPreviousItem() {
+   items[count].classList.remove('active');
+ 
+   if(count > 0) {
+     count--;
+   } else {
+     count = itemCount - 1;
+   }
+ 
+   items[count].classList.add('active');
+   console.log(count);
+ }
+ 
+ function keyPress(e) {
+   e = e || window.event;
+   
+   if (e.keyCode == '37') {
+     showPreviousItem();
+   } else if (e.keyCode == '39') {
+     showNextItem();
+   }
+ }
+ 
+ nextItem.addEventListener('click', showNextItem);
+ previousItem.addEventListener('click', showPreviousItem);
+ document.addEventListener('keydown', keyPress);
